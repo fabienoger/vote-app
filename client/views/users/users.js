609c1720e01c838fac81d52e55523374d23d7782
fabienoger <fab-oger@live.fr>
2016-03-30 15:24:54 +0200

    Improve users template and call method for update

--- client/views/users/users.js
+++ client/views/users/users.js
@@ -3,9 +3,45 @@ Template.users.rendered = function() {
   $('.ui.accordion').accordion();
 }
 
+Template.users.events({
+  // Display form on the right for updating user
+  'click .update-user': function(e, t) {
+    var user = null;
+    if (e.target.dataset.id) {
+      console.log(e.target.dataset.id);
+      // Get the user with the _id
+      user = Meteor.users.findOne({_id : e.target.dataset.id});
+      Modules.client.user.updateUser.set(user);
+    } else {
+//      console.log(e.target);
+    }
+  },
+  // Remove a user
+  'click .remove-user': function(e, t) {
+    if (e.target.dataset.id) {
+      console.log(e.target.dataset.id);
+    } else {
+//      console.log(e.target);
+    }
+  },
+  'submit #update-user': function(e, t) {
+    e.preventDefault;
+    
+  }
+});
+
 Template.users.helpers({
+  // Initialize checkbox in update-user form
+  'initializeCheckbox': function() {
+    // Initialize checkbox
+    console.log("Initialize checkbox");
+    $('.ui.toggle.checkbox').checkbox();
+  },
   // Return all users
   'users': function() {
     return Meteor.users.find({}).fetch();
+  },
+  'selectedUser': function() {
+    return Modules.client.user.updateUser.get();
   }
 });
