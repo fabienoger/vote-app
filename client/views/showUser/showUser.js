d77393c2b37da6602a88ef6a0cbee08eceb103c8
fabienoger <fab-oger@live.fr>
2016-04-01 11:34:33 +0200

    Add votes on showUser

--- client/views/showUser/showUser.js
+++ client/views/showUser/showUser.js
@@ -52,14 +52,22 @@ Template.showUser.events({
 *****************************************************************************/
 
 Template.showUser.helpers({
+  // Return votes for one user
+  getUserVotes: function() {
+    // Initialize variables
+    var userId = FlowRouter.getParam("id");
+    // Mongo request
+    var votes = Votes.find({usersId: userId});
+    return votes;
+  },
   // Get the mongo object for the param :id
   getUser: function() {
     // Get user
     var user = Meteor.users.findOne({_id: FlowRouter.getParam("id")});
     return user;
   },
-  // Return vote for one user
-  getUserVotes: function() {
+  // Return created votes for one user
+  getUserCreatedVotes: function() {
     // Initialize variables
     var userId = FlowRouter.getParam("id");
     // Mongo request
