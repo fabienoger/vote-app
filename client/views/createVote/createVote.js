c0f0bcd4e38cd8c0d954016179ea72408b10efec
fabienoger <fab-oger@live.fr>
2016-04-01 10:54:30 +0200

    Remove option on createVote

--- client/views/createVote/createVote.js
+++ client/views/createVote/createVote.js
@@ -22,14 +22,20 @@ Template.createVote.helpers({
 ****************************************/
 
 Template.createVote.events({
+  // Remove option
   // Remove option from ReactiveArray
   'click .ui.label.vote': function(e, t) {
     e.preventDefault;
     // set variables
-    var clickedOption = e.target.innerText.trim();
+    var clickedOption = e.target.dataset.name;
 
-    // Remove option from ReactiveArray
-    Modules.client.vote.newVote.options.remove(clickedOption);
+    // Browse reactive array
+    _.map(Modules.client.vote.newVote.options.list(), function(d, i) {
+      if (d.name == clickedOption) {
+        // ReactiveArray splice array
+        Modules.client.vote.newVote.options.splice(i, 1);
+      }
+    });
   },
   // Add option to ReactiveArray
   'click #new-vote-add-option': function(e, t) {
@@ -37,15 +43,22 @@ Template.createVote.events({
     // Set variables
     var voteOption = t.find('#new-vote-option');
     var voteOptionList = t.find('#new-vote-option');
-    if (voteOption.value.trim() != "") {
-      // Push new voteOption in ReactiveArray
-      Modules.client.vote.newVote.options.push({
-        name: voteOption.value.trim(),
-        voted: 0,
-        usersId: []
-      });
-      // Empty field #new-vote-option
-      voteOption.value = "";
+    var result = _.findWhere(Modules.client.vote.newVote.options.list(), {name: voteOption.value.trim()});
+
+    // Check if voteOption is already contains in array
+    if (!result) {
+      if (voteOption.value.trim() != "") {
+        // Push new voteOption in ReactiveArray
+        Modules.client.vote.newVote.options.push({
+          name: voteOption.value.trim(),
+          voted: 0,
+          usersId: []
+        });
+        // Empty field #new-vote-option
+        voteOption.value = "";
+      }
+    } else {
+      Modules.client.utils.displayPanel("vote-option-list-info", "negative", "warning", "The voting option exists.")
     }
   },
   // Create vote & insert in Votes collection
